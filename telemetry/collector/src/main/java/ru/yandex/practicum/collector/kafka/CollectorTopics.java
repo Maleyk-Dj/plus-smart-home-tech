b9a0002eb6f35e4d116d2276a76b97646fecc3d9
Maleyk-Dj <malika4872@ya.ru>
2025-11-19 15:28:31 +0300

    feat: разработала сервис aggregator (#9)
    Co-authored-by: Maleyk <m9375774872@gmail.com>

--- telemetry/collector/src/main/java/ru/yandex/practicum/collector/kafka/CollectorTopics.java
+++
@@ -1,7 +0,0 @@
-package ru.yandex.practicum.collector.kafka;
-
-
-public class CollectorTopics {
-    public final static String TELEMETRY_SENSORS_V1 = "telemetry.sensors.v1";
-    public final static String TELEMETRY_HUBS_V1 = "telemetry.hubs.v1";
-}
