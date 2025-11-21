5b7cd0d8d974936037ed67067e4a9abee232908f
Maleyk-Dj <malika4872@ya.ru>
2025-11-21 21:32:43 +0300

    Develop (#12)
    * 2 collector grpc (#8)
    
    * feat(proto-schemas): добавила protobuf схемы для событий датчиков и событий хаба
    
    * feat(proto-schemas): добавила protobuf схемы для событий датчиков и событий хаба
    
    * feat(proto-schemas): добавила protobuf схемы для событий датчиков и событий хаба
    
    * feat(proto-schemas): добавила protobuf схемы для событий датчиков и событий хаба
    
    ---------
    
    Co-authored-by: Maleyk <m9375774872@gmail.com>
    
    * feat: разработала сервис aggregator (#9)
    
    Co-authored-by: Maleyk <m9375774872@gmail.com>
    
    * feat: разработала сервис analyzer (#11)
    
    * feat: разработала сервис analyzer
    
    
    ---------
    
    Co-authored-by: Maleyk <m9375774872@gmail.com>
    
    ---------
    
    Co-authored-by: Maleyk <m9375774872@gmail.com>

---
+++ telemetry/aggregator/src/main/java/ru/yandex/practicum/Aggregator.java
@@ -0,0 +1,22 @@
+package ru.yandex.practicum;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
+import ru.yandex.practicum.config.AggregationStarter;
+
+/**
+ * Hello world!
+ */
+@SpringBootApplication
+@Slf4j
+public class Aggregator {
+    public static void main(String[] args) {
+        log.info("Launch AggregatorService");
+        ConfigurableApplicationContext context = SpringApplication.run(Aggregator.class, args);
+
+        AggregationStarter aggregator = context.getBean(AggregationStarter.class);
+        aggregator.start();
+    }
+}
---
+++ telemetry/analyzer/src/main/java/ru/yandex/practicum/entity/ScenarioCondition.java
@@ -0,0 +1,48 @@
+package ru.yandex.practicum.entity;
+
+
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.EmbeddedId;
+import jakarta.persistence.Entity;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.MapsId;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+import org.apache.kafka.common.metrics.Sensor;
+
+@Entity
+@Table(name = "scenario_conditions")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
+@Builder
+public class ScenarioCondition {
+    @EmbeddedId
+    private ScenarioConditionId id;
+
+    @ManyToOne
+    @JoinColumn(name = "scenario_id")
+    @MapsId(value = "scenarioId")
+    @ToString.Exclude
+    private Scenario scenario;
+
+    @ManyToOne
+    @JoinColumn(name = "sensor_id")
+    @MapsId(value = "sensorId")
+    @ToString.Exclude
+    private HomeSensor sensor;
+
+    @ManyToOne(cascade = CascadeType.ALL)
+    @JoinColumn(name = "condition_id")
+    @MapsId(value = "conditionId")
+    @ToString.Exclude
+    private Condition condition;
+}
